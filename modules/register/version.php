a9e606f150adde608d1ec8e81ba63a74209b0115
duykhanh14 <khanhnd@vatgia.com>
2022-12-06 22:59:53 +0700

    create module register

---
+++ modules/register/version.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * NukeViet Content Management System
+ * @version 4.x
+ * @author VINADES.,JSC <contact@vinades.vn>
+ * @copyright (C) 2009-2021 VINADES.,JSC. All rights reserved
+ * @license GNU/GPL version 2 or any later version
+ * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ */
+
+if (!defined('NV_ADMIN') or !defined('NV_MAINFILE')) {
+    exit('Stop!!!');
+}
+
+$module_version = [
+    'name' => 'Register',
+    'modfuncs' => '',
+    'is_sysmod' => 0,
+    'virtual' => 1,
+    'version' => '1.0.00',
+    'date' => 'Tuesday, Dec 6, 2022 11:00:00 PM GMT+07:00',
+    'author' => 'DuyKhanh <ngoduykhanh2001@gmail.com>',
+    'note' => '',
+    'uploads_dir' => [
+
+    ]
+];
